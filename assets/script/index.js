6e3635aff752fd0b15225644965b872d807d4831
Paul Funston <paulfunston9@gmail.com>
2022-11-07 14:16:18 -0600

    complete JS

--- assets/script/index.js
+++ assets/script/index.js
@@ -5,3 +5,24 @@
 
     Utility Functions
 */
+
+function onEvent(event, selector, callback) {
+  return selector.addEventListener(event, callback);
+}
+
+function select(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+
+const button = select('.show-modal');
+const modal = select('.modal');
+const box = select('.white-box');
+
+onEvent('click', button, () => {
+  modal.classList.remove('hidden');
+});
+
+onEvent('click', box, () => {
+  modal.classList.add('hidden');
+})
